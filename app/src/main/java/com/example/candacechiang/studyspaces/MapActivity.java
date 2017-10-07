a7b5a96f4c5571b0329282af63d5866bc685bef9
Candace Chiang <chiang.candace@berkeley.edu>
2017-10-07 14:26:22 -0700

    don't trust this idk what i'm doing??

---
+++ app/src/main/java/com/example/candacechiang/studyspaces/MapActivity.java
@@ -0,0 +1,39 @@
+package com.example.candacechiang.studyspaces;
+
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
+
+public class MapActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Intent intent = null;
+        if (id == R.id.about_help) {
+            intent = new Intent(this, AboutHelp.class);
+            startActivity(intent);
+        } //make correct!
+
+        startActivity(intent);
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
